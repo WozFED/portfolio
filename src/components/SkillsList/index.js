3043f2a5571ce464b6495337cf18af3f6fe40f85
WozFED <woztrn@gmail.com>
2021-11-27 08:58:56 +0100

    testing second view more classic

---
+++ src/components/SkillsList/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const SkillsList = () => {
+    return (
+        <div className = "skills">
+            <div className = "skills__wrapper">
+        <div className = "skills__content lang">
+        <ul>
+            <li><div className = "skills__element">
+                <p>HTML</p>
+                <div className = "skills__element-bar">
+                <div></div>
+                </div>
+                </div></li>
+                <li><div className = "skills__element">
+                <p>CSS</p>
+                <div className = "skills__element-bar">
+                <div></div>
+                </div>
+                </div></li>
+                <li><div className = "skills__element">
+                <p>JS</p>
+                <div className = "skills__element-bar">
+                <div></div>
+                </div>
+                </div></li>
+
+        </ul></div>
+        
+        <div className = "skills__content frameworks">
+        <ul>
+            <li><div className = "skills__element">
+                <p>HTML</p>
+                <div className = "skills__element-bar">
+                <div></div>
+                </div>
+                </div></li>
+                <li><div className = "skills__element">
+                <p>CSS</p>
+                <div className = "skills__element-bar">
+                <div></div>
+                </div>
+                </div></li>
+                <li><div className = "skills__element">
+                <p>JS</p>
+                <div className = "skills__element-bar">
+                <div></div>
+                </div>
+                </div></li>
+
+        </ul></div></div></div>
+    )
+}
+
+export default SkillsList
---
+++ src/pages/contact.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ContactPage = () => {
+    return (
+        <div className = "test">
+            <div className = "test__wrapper">
+                
+               <div className = "navigation">
+                   <div className = "navigation__options">
+                       <ul>
+                           <li>Język</li>
+                           <li>Motyw</li>
+                       </ul>
+                   </div>
+                   <div className = "navigation__list">
+                      <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Skills</li>
+                        <li>Projects</li>
+                        <li>Contact</li>
+                    </ul> 
+                   </div>
+                    
+                </div> 
+            </div>
+            
+        </div>
+    )
+}
+
+export default ContactPage
