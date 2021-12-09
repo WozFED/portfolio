cb31aedb970701737e572f28f38dcc81d8e0fd41
WozFED <woztrn@gmail.com>
2021-12-09 15:23:02 +0100

    no message

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,14 +4,13 @@ import Layout from "../components/Layout"
 import Header from '../components/Header'
 import Table from "../components/Table"
 
-const ContactPage = ({ data }) => {
-  const table = data.allContactJson.nodes
+const ContactPage = () => {
   return (
     <Layout>
       <div className="page ">
         <div className="page__wrapper contact">
           <Header h2={"Contact me"} p={"Here many ways to contact with me"} />
-          <Table table = {table}/>
+          <Table />
         </div>
       </div>
     </Layout>
